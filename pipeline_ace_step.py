fb61d6d676d0aa1522f0179625b2e969cef8e7fc
camenduru <54370274+camenduru@users.noreply.github.com>
2025-08-09 19:12:20 +0300

    test

--- pipeline_ace_step.py
+++ pipeline_ace_step.py
@@ -2,7 +2,6 @@ import random
 import time
 import os
 import re
-import spaces
 import torch
 import torch.nn as nn
 from loguru import logger
@@ -1447,7 +1446,6 @@ class ACEStepPipeline:
         latents, _ = self.music_dcae.encode(input_audio, sr=sr)
         return latents
 
-    @spaces.GPU
     def __call__(
         self,
         audio_duration: float = 60.0,
